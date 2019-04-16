75b544575a6c7227202c3a4d9d009c0881a20d65
Cheng <cl344@duke.edu>
2019-04-16 19:37:52 -0400

    debuged version

--- init.py
+++ init.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 16 13:48:41 2019
 
-@author: zhangjing
-"""
-
-import pandas as pd
 import numpy as np
 
 import keras
@@ -14,6 +6,11 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, GlobalMaxPooling2D
 from keras.layers import Flatten, Dense, Dropout, Activation 
 from keras.preprocessing.image import ImageDataGenerator
+from keras.models import model_from_json
+from PIL import Image
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+import time
 
 def modelWithInit(init):
     model = Sequential()
@@ -28,7 +25,8 @@ def modelWithInit(init):
 
     model.add(Conv2D(80, (3, 3), padding='same', kernel_initializer = init, activation = 'relu'))
     model.add(Conv2D(80, (3, 3), padding='same', kernel_initializer = init, activation = 'relu'))
-    model.add(Conv2D(80, (3, 3), padding='same', kernel_initializer = init, activation = 'relu')) model.add(Conv2D(80, (3, 3), padding='same', input_shape = (224, 224, 3), activation = 'relu'))
+    model.add(Conv2D(80, (3, 3), padding='same', kernel_initializer = init, activation = 'relu')) 
+    model.add(Conv2D(80, (3, 3), padding='same', kernel_initializer = init, activation = 'relu'))
     model.add(Conv2D(80, (3, 3), padding='same', kernel_initializer = init, activation = 'relu'))
     model.add(MaxPooling2D(pool_size = (2, 2)))
     model.add(Dropout(0.25))
@@ -67,10 +65,10 @@ def training(model):
     train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
     test_datagen = ImageDataGenerator(rescale = 1./255)
     
-    train_generator = train_datagen.flow_from_directory('./train_set/', target_size = (224, 224), color_mode = "rgb", batch_size = 32,
+    train_generator = train_datagen.flow_from_directory('train_small/', target_size = (224, 224), color_mode = "rgb", batch_size = 32,
                                                  class_mode = 'categorical', shuffle = True, seed = 2019);
                                                         
-    valid_generator = test_datagen.flow_from_directory('./valid_set/', target_size = (224, 224), color_mode = "rgb", batch_size = 32, 
+    valid_generator = test_datagen.flow_from_directory('valid_small/', target_size = (224, 224), color_mode = "rgb", batch_size = 32, 
                                                        class_mode = 'categorical', shuffle = True, seed = 2019);
                                                        
     STEP_SIZE_TRAIN = train_generator.n//train_generator.batch_size;
@@ -81,7 +79,7 @@ def training(model):
                         validation_data = valid_generator, validation_steps = STEP_SIZE_VALID, 
                         callbacks=[time_callback]);
     times = time_callback.times
-    return (model.history, times)
+    return (model, times)
 
 # Four different initialization methods
 init0 = keras.initializers.glorot_normal(seed = 2019)
@@ -94,7 +92,10 @@ t = 0
 for init in inits:
     t = t + 1
     model = modelWithInit(init)
-    history, times = training(model)
+    model, times = training(model)
+
+
+    history=model.history
     print("initialization method {}".format(t))
 
     print(history['acc'])
@@ -103,6 +104,12 @@ for init in inits:
     print(history['loss'])
     print(history['val_loss'])
 
-    print("initial validation accuracy:".format(history['val_acc'][0])
+    print("initial validation accuracy: {}".format(history['val_acc'][0]))
     print("total training time: {}".format(np.sum(times)))
     print("total training time: {}".format(np.average(times)))
+
+    # serialize model to JSON
+    model_json = model.to_json()
+    modelname = "m"+str(t)+".json"
+    with open(modelname, "w") as json_file:
+       json_file.write(model_json)
